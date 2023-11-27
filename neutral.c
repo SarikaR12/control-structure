78e9e5553fac7a00abaff50e9c76ba319d8ddae3
SarikaR12 <143924688+SarikaR12@users.noreply.github.com>
2023-11-27 10:31:20 +0530

    Add files via upload

---
+++ neutral.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+
+{
+	int neutral;
+	printf("enter value neutral=");
+	scanf("%d",&neutral);
+	
+	if(neutral>0)
+	{
+		printf("number is not neutral");
+	}
+	else if(neutral==0)
+	{
+		printf("numer is neutral");
+	}
+	else if(neutral<0)
+	{
+		printf("number is not neutral");
+	}
+}
